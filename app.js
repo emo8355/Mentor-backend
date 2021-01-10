74b7b94f21304c0f230f25951b44bb46cd72629b
homer li <homer.lee.0508@gmail.com>
2021-01-10 02:17:04 -0800

    Access-Control

--- app.js
+++ app.js
@@ -10,9 +10,17 @@ module.exports = () => {
 	const businessRouter = require("./routes/businessController")();
 	const UserRouter = require("./routes/ticketController")();
 	const workshopRouter = require("./routes/workshopController")();
-	const packageRouter = require('./routes/packageController')();
-	const categoryRouter = require('./routes/categoryController')();
-  
+	const packageRouter = require("./routes/packageController")();
+	const categoryRouter = require("./routes/categoryController")();
+
+	app.use(function (req, res, next) {
+		res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept"
+		);
+		next();
+	});
 	app.use(express.static(path.join(__dirname, "public")));
 	app.use(compression());
 
@@ -40,10 +48,9 @@ module.exports = () => {
 	app.use("/business", businessRouter);
 	app.use("/ticket", UserRouter);
 	app.use("/workshop", workshopRouter);
-	app.use('/package', packageRouter);
-	app.use('/workshop', workshopRouter);
-	app.use('/category', categoryRouter);
-
+	app.use("/package", packageRouter);
+	app.use("/workshop", workshopRouter);
+	app.use("/category", categoryRouter);
 
 	return app;
 };
--- routes/workshopController.js
+++ routes/workshopController.js
@@ -7,70 +7,59 @@ const Mongoose = require("mongoose");
 
 const AWS = require("../middleware/imageUpload");
 
-
 module.exports = () => {
 	workshopController.get("/", (req, res) => {});
 
 	workshopController.get("/all", (req, res) => {
-		const dbWorkshops = Workshop.find();
-		res.json({
-			msg: "All workshops",
-			data: dbWorkshops,
+		Workshop.find({}, (err, data) => {
+			if (err) {
+				res.status = 400;
+				res.json({
+					message: "error",
+					err: err,
+				});
+			}
+			res.json({
+				msg: "All workshops",
+				data: data,
+			});
 		});
 	});
 
-    workshopController.get('/all',  (req,res)=>{
-       Workshop.find({},(err, data)=>{
-            if (err) {
-                res.status = 400;
-                res.json({
-                    message: "error",
-                    err: err
-                })
-            }
-            res.json({
-                msg: "All workshops",
-                data: data,
-            })
-       });
-       
-    })
+	workshopController.get("/:id", (req, res) => {
+		console.log(req.params.id);
+		Workshop.find({ _id: req.params.id }, (err, data) => {
+			if (err) {
+				res.status = 400;
+				return res.json({
+					message: "error",
+					err: err,
+				});
+			}
 
-    workshopController.get('/:id',  (req,res)=>{
-        console.log(req.params.id);
-        Workshop.find({_id : req.params.id}, (err, data)=> {
-            console.log(data)
-            if (err) {
-                res.status = 400;
-                res.json({
-                    message: "error",
-                    err: err
-                })
-            }
+			return res.json({
+				msg: "One workshop",
+				data: data,
+			});
+		});
+	});
 
-            res.json({
-                msg: "One workshop",
-                data: data,
-            })
-        });
-    })
+	workshopController.get("/business/:id", (req, res) => {
+		Workshop.find({ businessID: req.params.id }, (err, data) => {
+			if (err) {
+				res.status = 400;
+				res.json({
+					message: "error",
+					err: err,
+				});
+			}
 
-    workshopController.get('/business/:id',  (req,res)=>{
-        Workshop.find({businessID : req.params.id}, (err, data)=>{
-            if (err) {
-                res.status = 400;
-                res.json({
-                    message: "error",
-                    err: err
-                })
-            }
-
-            res.json({
-                msg: "Get workshops by business id",
-                data: data,
-            })
-        });
-    })
+			res.json({
+				msg: "Get workshops by business id",
+				data: data,
+			});
+		});
+	});
 
 	workshopController.post("/update-info/:id", (req, res) => {
 		Workshop.updateOne(
@@ -118,72 +107,74 @@ module.exports = () => {
 		});
 	});
 
-    workshopController.post('/update-info/:id',  (req,res)=>{
-        Workshop.updateOne(
-            {_id: req.params.id},
-            {$set:{
-                name: req.body.name,
-                description: req.body.description,
-                mentor_name : req.body.mentor_name,
-                start_time: req.body.start_time,
-                end_time: req.body.end_time,
-                location : req.body.location,
-                package_required: req.body.package_required,
-                price : req.body.price,
-                is_active : req.body.is_active,
-                categoryID : req.body.categoryID,
-                max_attendance : req.body.max_attendance,
-                min_attendance : req.body.min_attendance, 
-            }},
-            )
-            .then((data)=>{
-                console.log('workshop info updated' + data);
-            })
-            .catch(err=>{
-                console.log(err)
-            })
-    })
+	workshopController.post("/update-info/:id", (req, res) => {
+		Workshop.updateOne(
+			{ _id: req.params.id },
+			{
+				$set: {
+					name: req.body.name,
+					description: req.body.description,
+					mentor_name: req.body.mentor_name,
+					start_time: req.body.start_time,
+					end_time: req.body.end_time,
+					location: req.body.location,
+					package_required: req.body.package_required,
+					price: req.body.price,
+					is_active: req.body.is_active,
+					categoryID: req.body.categoryID,
+					max_attendance: req.body.max_attendance,
+					min_attendance: req.body.min_attendance,
+				},
+			}
+		)
+			.then((data) => {
+				console.log("workshop info updated" + data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
 
-   
-    workshopController.post(AWS.upload.single("pic"), (req, res) => {
-        const infoBody = { ...req.body };
-        const file = req.file;
-        let img_url = `https://d39wlfkh0mxxlz.cloudfront.net/${file.originalname}`;
-        console.log(img_url);
-        console.log(infoBody);
-        res.send("ojbk");
-    });
-    
+	workshopController.post(AWS.upload.single("pic"), (req, res) => {
+		const infoBody = { ...req.body };
+		const file = req.file;
+		let img_url = `https://d39wlfkh0mxxlz.cloudfront.net/${file.originalname}`;
+		console.log(img_url);
+		console.log(infoBody);
+		res.send("ojbk");
+	});
 
-    workshopController.post('/update-attendance/:id/guest/:guestid',  (req,res)=>{
-       Workshop.updateOne(
-        {_id: req.params.id},
-        {$push:{curr_attendance: req.params.guestid}},
-        )
-        .then((data)=>{
-            console.log('attendance updated' + data);
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-    })
+	workshopController.post(
+		"/update-attendance/:id/guest/:guestid",
+		(req, res) => {
+			Workshop.updateOne(
+				{ _id: req.params.id },
+				{ $push: { curr_attendance: req.params.guestid } }
+			)
+				.then((data) => {
+					console.log("attendance updated" + data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	);
 
-    workshopController.post('/delete/:id',  (req,res)=>{
-       Workshop.deleteOne({_id: req.params.id}, (err)=>{
-           if (err) {
-               res.status = 400;
-               res.json({
-                   message: "Workshop could not be deleted",
-                   err : err
-               })
-           }
-           res.json({
-               message: "Workshop has been deleted"
-           })
-       });
-    })
-  
-  
+	workshopController.post("/delete/:id", (req, res) => {
+		Workshop.deleteOne({ _id: req.params.id }, (err) => {
+			if (err) {
+				res.status = 400;
+				res.json({
+					message: "Workshop could not be deleted",
+					err: err,
+				});
+			}
+			res.json({
+				message: "Workshop has been deleted",
+			});
+		});
+	});
+
 	workshopController.post("/:id/createTicket", (req, res) => {
 		const ticketInfo = req.body;
 		const workshopId = req.params.id;
@@ -205,8 +196,10 @@ module.exports = () => {
 
 			Workshop.updateOne(
 				{ _id: req.params.id },
-				{ $push: { curr_attendance: doc.id } },
-				{ $inc: { numberOfPeople: ticketInfo.people } }
+				{
+					$push: { curr_attendance: doc.id },
+					$inc: { numberOfPeople: Number(ticketInfo.people) },
+				}
 			)
 				.then((data) => {
 					console.log("number of attendance updated" + data);
